55d114a9eaa28c305ec3445f8994e2110d1b7b99
ROCK3D3L <roquedelvalle@unc.edu.py>
2020-03-24 21:42:18 -0400

    Inicio de login

---
+++ administacion.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+     <head>
+          <script src="js/darck.js"></script>
+          <script type="text/javascript">
+                DarkReader.enable({
+                     brightness: 100,
+                     contrast: 100,
+                     sepia: 10
+                });
+                // DarkReader.disable(); // PARA CANCELAR O DETENERLO // IDEA: PODENOS PONER UN BOTON EN ALGUN LUGAR PARA ELLO ALGUN DIA....
+          </script>
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+          <meta name="theme-color" content="black">
+          <title>Hotel Rivera del Norte</title>
+          <link rel="icon" type="image/png" href="img/icono.jpeg"/>
+          <link rel="stylesheet" href="bt/bootstrap.min.css">
+          <link rel="stylesheet" href="css/estilos.css">
+          <link rel="stylesheet" href="alertify/alertify.min.css">
+          <script src="js/jquery-3.3.1.min.js"></script>
+          <script src="alertify/alertify.min.js"></script>
+          <!-- Boostrap -->
+          <script src="bt/bootstrap.min.js"></script>
+          <!-- Font-Adswesome -->
+          <link rel="stylesheet" href="font-awesome/font-awesome.min.css">
+          <style media="screen">
+               #panelAcceso{
+                   max-width: 400px;
+                   margin: auto;
+                   margin-top: 3%;
+                   border:1px solid;
+                   border-color: #FF0000!important;
+                   box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075), 0 0 8px rgba(255, 0, 0, 0.6)!important;
+                   text-align:center;
+               }
+
+               .form-control:focus {
+                   border-color: #FF0000!important;
+                   box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075), 0 0 8px rgba(255, 0, 0, 0.6)!important;
+
+               }
+               #logo{
+                   width: 70%;
+               }
+
+          </style>
+          <!-- TEMA OSCURO -->
+
+     </head>
+     <body class="container">
+          <?php session_start(); ?>
+          <script>
+              $(document).ready(function(){
+                  var usuValido = "<?php echo isset($_SESSION['usuarioValido']) ? $_SESSION['usuarioValido'] : '0'; ?>";
+                  var usuNivel  = "<?php echo isset($_SESSION['nivelUsuario']) ? $_SESSION['nivelUsuario'] : '0'; ?>";
+                  if(usuValido == 'no'){
+                       alertify.error("El Usuario, contraseña o Sucusal incorrecta!!!");
+                  }else if(usuValido == 'noo'){
+                       alertify.error("El usuario se encuentra logeado o desactivado actualmente!!!");
+                  }else if (usuValido == "si"){
+                      //if(usuNivel == "ADMINISTRATIVO"){
+                          window.location="menuadmin.php";
+                     // }else if(usuNivel == "INVENTARIO"){
+                     //      window.location="menuinventa.php";
+                     // }else if (usuNivel == "VENTA") {
+                     //      window.location="menuvende.php";
+                     // }
+                  }
+              });
+          </script>
+
+          <div id="panelAcceso" class="panel panel-danger">
+              <div class="panel-heading bg-danger">
+                  <h1 id="titulo" class="panel-title text-center"><b>Acceso al Sistema</b></h1>
+              </div>
+              <div class="panel-body ">
+                <div class="col-md-12">
+                  <img id="logo" src="img/logo.jpeg" alt=""/>
+                </div>
+                  <form style="background-color:rgba(25, 26, 27,0.95);" id="formAcceso" method="post" action="servicios/validarAcceso.php">
+                      <div class="row" align="center">
+                      </div>
+                      <br>
+                      <div class="form-group row">
+                          <div class="col-12">
+                              <div style="color: white;" class="form-group">
+                                  <div class="input-group col-12">
+                                       <div class="input-group-addon" style="border: 1px solid; margin: auto; padding: 6px; border-color: #FF0000!important;width: 40px;
+                                       box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075), 0 0 8px rgba(255, 0, 0, 0.6)!important;">
+                                            <i class="fa fa-user-circle"></i>
+                                       </div>
+     							 <input class="form-control" placeholder="Usuario" id="loginname" name="loginname" type="number" autocomplete="off" min="0" autofocus/>
+
+                                  </div>
+                                  <br>
+                                  <div class="input-group col-12 ">
+                                       <div class="input-group-addon" style="border: 1px solid; margin: auto; padding: 6px; border-color: #FF0000!important; width: 40px;
+                                       box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075), 0 0 8px rgba(255, 0, 0, 0.6)!important;">
+                                            <i class="fa fa-lock"></i>
+                                       </div>
+                                      <input class="form-control" placeholder="Contraseña" id="password" name="password" type="password" value="" onkeypress="enter(event)">
+
+                                  </div>
+                                  <br>
+                                  <div class="input-group col-12 ">
+                                       <div class="input-group-addon" style="border: 1px solid; margin: auto; padding: 6px; border-color: #FF0000!important; width: 40px;
+                                       box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075), 0 0 8px rgba(255, 0, 0, 0.6)!important;">
+                                            <i class="fa fa-map-marker"></i>
+                                       </div>
+                                       <select id="sucursal" name="sucursal" class="form-control">
+                                          <option value="">Seleccione una sucursal</option>
+                                          <?php
+                                                 require_once("servicios/conexion.php");
+                                                 $conex = conexion();
+                                                 $sql = "SELECT Cod_sucursal,Acronimo FROM sucursal";
+                                                 $res = mysqli_query($conex, $sql);
+                                                 while ($row = mysqli_fetch_array($res)) {
+                                                   echo '  <option value="'.$row["Cod_sucursal"].'">'.$row["Acronimo"].'</option>';
+                                                 }
+                                             ?>
+                                      </select>
+
+                                  </div>
+                                  <br>
+                                  <div class="col-12">
+                                       <button type="button" onclick="validarCampos();" id="botonIngresar" class="btn btn-lg btn-danger btn-block"><b>Ingresar</b>
+                                       </button>
+                                  </div>
+
+                              </div>
+                          </div>
+                      </div>
+                  </form>
+              </div>
+          </div>
+
+          <script type="text/javascript">
+
+
+              function validarCampos() {
+                 if($("#loginname").val()===""){
+                      alertify.error("Usuario no puede estar vacio.");
+                      $("#loginname").focus();
+                  }else if($("#password").val()===""){
+                     alertify.error("Contraseña no puede estar vacio.");
+                      $("#password").focus();
+                  }else if($("#sucursal").val()===""){
+                     alertify.error("Debe seleccionar sucursal.");
+                      $("#sucursal").focus();
+                  }else{
+                      $("#formAcceso").submit();
+                  }
+              }
+              //--------------------------------------------------------------------------------------
+              function enter(e) {
+                  tecla = (document.all) ? e.keyCode : e.which;
+                  if (tecla==13){
+                      validarCampos();
+                  }
+              }
+              //--------------------------------------------------------------------------------------
+          </script>
+
+
+     </body>
+</html>
