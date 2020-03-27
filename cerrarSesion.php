445c3448d28d9ab0d3a2f8833da515b1096b1c31
ROCK3D3L <roquedelvalle@unc.edu.py>
2020-03-26 20:57:30 -0400

    Revert "ALFIN CARAJO"
    This reverts commit 0b0d14aaacb63b44637d9e2ed634c67fa448e282.

--- cerrarSesion.php
+++
@@ -1,9 +0,0 @@
-<?php
-		session_start(); 	//Iniciamos o Continuamos la sesion
-		require_once("conexcion.php");
-		$conexion = null;
-		$conexion = conectar();
-		session_unset();	//Elimina informaciones de todas las sesiones
-		session_destroy();	//Cierra la sesion
- 		header("Location:administracion.php");
-?>
